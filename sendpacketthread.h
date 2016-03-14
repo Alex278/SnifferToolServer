38e5d0e129df72e7b628e5728a4a25f1f00a3421
FreeApe <freeape@aliyun.com>
2016-03-14 23:17:13 +0800

    add sendpacket thread

---
+++ sendpacketthread.h
@@ -0,0 +1,49 @@
+#ifndef SENDPACKETTHREAD_H
+#define SENDPACKETTHREAD_H
+
+#include <QObject>
+#include <QThread>
+#include "pcap.h"
+#include "tcpipcommon.h"
+#include "tcpipprotocol.h"
+
+class SendPacketThread : public QThread
+{
+    Q_OBJECT
+
+public:
+    //
+    SendPacketThread();    
+    //
+    SendPacketThread(pcap_t *handle,HostInfo *hostInfo,u_short type,QString ips,QString ipe);
+    //
+    ~SendPacketThread();
+
+    // 获取packet 对象
+//    template <typename T>
+//    T &getPacket(u_short type){
+//        if(type == ARP_PACKET)
+//            return arppacket;
+//        if(type == TCP_PACKET)
+//            return tcppacket;
+//    }
+    // 获取arppacket
+    YArpPacket* &getArpPacket();
+    // 发送ARP扫描包
+    void sendArpScanPacket();
+    // 发送ARP欺骗包
+    void sendArpCheatPacket();
+    // 线程运行函数
+    void run();
+private:
+    pcap_t *handle;
+    HostInfo *hostInfo;
+    u_short type;
+    QString ipStart;
+    QString ipEnd;
+
+    YArpPacket *arppacket;
+    YTcpPacket *tcppacket;
+};
+
+#endif // SENDPACKETTHREAD_H
